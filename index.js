57fe9f7e7443557aaf3a4eafa679f07ee2ce6e2e
bbastanza <b.bastanza@gmail.com>
2020-08-04 22:43:03 -0700

    commit math functionality

--- index.js
+++ index.js
@@ -1,34 +1,72 @@
-let buttons = document.getElementsByClassName("number");
+const box = document.getElementById("box");
 
-let box = document.getElementById("box");
+const numberKeys = document.getElementsByClassName("number");
 
-for (let i = 0; i < buttons.length; i++) {
-  const button = buttons[i];
-  button.addEventListener("click", function (e) {
-    document.getElementById("box").value = document.getElementById("box").value + e.target.value;
-  });
+for (let i = 0; i < numberKeys.length; i++) {
+        const button = numberKeys[i];
+        button.addEventListener("click", function (e) {
+                box.value = box.value + e.target.value;
+        });
 }
 
+const functionKeys = document.getElementsByClassName("function");
+let expression = {
+        firstNumber: "",
+        method: "",
+        secondNumber: "",
+};
+
+for (let i = 0; i < functionKeys.length; i++) {
+        const method = functionKeys[i];
+        method.addEventListener("click", function (e) {
+                expression.firstNumber = box.value;
+                expression.method = method.id;
+                box.value = "";
+        });
+}
+
+document.getElementById("equal").addEventListener("click", () => {
+        expression.secondNumber = box.value;
+        let result;
+        if (expression.method === "plus") {
+                result = addNumbers(expression.firstNumber, expression.secondNumber);
+        } else if (expression.method === "minus") {
+                result = subtractNumbers(expression.firstNumber, expression.secondNumber);
+        } else if (expression.method === "multiply") {
+                result = multiplyNumbers(expression.firstNumber, expression.secondNumber);
+        } else if (expression.method === "divide") {
+                result = divideNumbers(expression.firstNumber, expression.secondNumber);
+        }
+        expression.firstNumber = result;
+});
+
 function addNumbers(number1, number2) {
-  const result = parseFloat(number1) + parseFloat(number2);
-  box.value = result;
-  return result;
+        const result = parseFloat(number1) + parseFloat(number2);
+        box.value = result;
+        return result;
 }
 
 function subtractNumbers(number1, number2) {
-  const result = parseFloat(number1) - parseFloat(number2);
-  box.value = result;
-  return result;
+        const result = parseFloat(number1) - parseFloat(number2);
+        box.value = result;
+        return result;
 }
 
 function multiplyNumbers(number1, number2) {
-  const result = parseFloat(number1) * parseFloat(number2);
-  box.value = result;
-  return result;
+        const result = parseFloat(number1) * parseFloat(number2);
+        box.value = result;
+        return result;
 }
 
 function divideNumbers(number1, number2) {
-  const result = parseFloat(number1) / parseFloat(number2);
-  box.value = result;
-  return result;
+        const result = parseFloat(number1) / parseFloat(number2);
+        box.value = result;
+        return result;
 }
+
+document.getElementById("clear").addEventListener("click", () => {
+        expression.firstNumber = "";
+        expression.method = "";
+        expression.secondNumber = "";
+        box.value = "";
+});
