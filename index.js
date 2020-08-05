8c95ca6aec9b50a5935598302cc559dec0da4b94
bbastanza <b.bastanza@gmail.com>
2020-08-05 12:04:02 -0700

    commit continuous functions

--- index.js
+++ index.js
@@ -1,72 +1,110 @@
+document.addEventListener("DOMContentLoaded", () => {
+        clearBoxValue();
+});
+document.getElementById("clear").addEventListener("click", () => {
+        expression.firstNumber = "";
+        expression.secondNumber = "";
+        expression.method = "";
+        expression.isGoing = true;
+        clearBoxValue();
+});
+
+function clearBoxValue() {
+        box.value = "";
+}
+
 const box = document.getElementById("box");
 
 const numberKeys = document.getElementsByClassName("number");
 
+let expression = {
+        firstNumber: "",
+        secondNumber: "",
+        isGoing: true,
+        method: "",
+};
+
 for (let i = 0; i < numberKeys.length; i++) {
         const button = numberKeys[i];
         button.addEventListener("click", function (e) {
-                box.value = box.value + e.target.value;
+                if (expression.isGoing === true) {
+                        box.value = box.value + e.target.value;
+                } else {
+                        box.value = e.target.value;
+                        expression.isGoing = !expression.isGoing;
+                }
         });
 }
 
-const functionKeys = document.getElementsByClassName("function");
-let expression = {
-        firstNumber: "",
-        method: "",
-        secondNumber: "",
-};
+document.getElementById("plus").addEventListener("click", () => {
+        displayAnswer();
+        expression.method = "plus";
+});
 
-for (let i = 0; i < functionKeys.length; i++) {
-        const method = functionKeys[i];
-        method.addEventListener("click", function (e) {
-                expression.firstNumber = box.value;
-                expression.method = method.id;
-                box.value = "";
-        });
-}
+document.getElementById("minus").addEventListener("click", () => {
+        displayAnswer();
+        expression.method = "minus";
+});
+
+document.getElementById("multiply").addEventListener("click", () => {
+        displayAnswer();
+        expression.method = "multiply";
+});
+
+document.getElementById("divide").addEventListener("click", () => {
+        displayAnswer();
+        expression.method = "divide";
+});
 
 document.getElementById("equal").addEventListener("click", () => {
-        expression.secondNumber = box.value;
-        let result;
-        if (expression.method === "plus") {
-                result = addNumbers(expression.firstNumber, expression.secondNumber);
-        } else if (expression.method === "minus") {
-                result = subtractNumbers(expression.firstNumber, expression.secondNumber);
-        } else if (expression.method === "multiply") {
-                result = multiplyNumbers(expression.firstNumber, expression.secondNumber);
-        } else if (expression.method === "divide") {
-                result = divideNumbers(expression.firstNumber, expression.secondNumber);
-        }
-        expression.firstNumber = result;
+        displayAnswer();
+        expression.method = "";
 });
 
 function addNumbers(number1, number2) {
         const result = parseFloat(number1) + parseFloat(number2);
         box.value = result;
-        return result;
+        updateExpression(result);
 }
 
 function subtractNumbers(number1, number2) {
         const result = parseFloat(number1) - parseFloat(number2);
         box.value = result;
-        return result;
+        updateExpression(result);
 }
 
 function multiplyNumbers(number1, number2) {
         const result = parseFloat(number1) * parseFloat(number2);
         box.value = result;
-        return result;
+        updateExpression(result);
 }
 
 function divideNumbers(number1, number2) {
         const result = parseFloat(number1) / parseFloat(number2);
         box.value = result;
-        return result;
+        updateExpression(result);
 }
 
-document.getElementById("clear").addEventListener("click", () => {
-        expression.firstNumber = "";
-        expression.method = "";
+function displayAnswer() {
+        if (expression.firstNumber === "") {
+                expression.firstNumber = parseFloat(box.value);
+                expression.isGoing = false;
+                return;
+        }
+        expression.secondNumber = parseFloat(box.value);
+        if (expression.method === "plus") {
+                addNumbers(expression.firstNumber, expression.secondNumber);
+        } else if (expression.method === "minus") {
+                subtractNumbers(expression.firstNumber, expression.secondNumber);
+        } else if (expression.method === "multiply") {
+                multiplyNumbers(expression.firstNumber, expression.secondNumber);
+        } else if (expression.method === "divide") {
+                divideNumbers(expression.firstNumber, expression.secondNumber);
+        }
+}
+
+function updateExpression(number) {
+        expression.isGoing = false;
+        expression.firstNumber = number;
         expression.secondNumber = "";
-        box.value = "";
-});
+}
