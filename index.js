db67201b57b0ec7713004e4ee9ed53cf9d35f735
bbastanza <b.bastanza@gmail.com>
2020-08-07 13:27:39 -0700

    commit adding back singlekeyfunctions

--- index.js
+++ index.js
@@ -27,7 +27,30 @@ const expression = {
     continuousText: true,
     method: "",
     memoryNumber: 0,
+    decimalInValue: false,
+    editFirstNumber: true,
 };
+
+const singleKeyExpression = {
+    firstNumberPi: false,
+    firstNumberNegative: false,
+    firstNumberPercent: false,
+    firstNumberSquareRoot: false,
+    secondNumberPi: false,
+    secondNumberNegative: false,
+    secondNumberPercent: false,
+    secondNumberSquareRoot: false,
+};
+
+const decimal = document.getElementById("decimal").addEventListener("click", () => {
+    if ((box.value = "")) {
+        box.value = ".";
+        expression.decimalInValue = true;
+    } else if (!expression.decimalInValue) {
+        box.value = box.value + ".";
+        expression.decimalInValue = true;
+    }
+});
 
 // loops through number keys and displays them on the screen
 const numberKeys = document.getElementsByClassName("number");
@@ -104,14 +127,24 @@ function displayAnswer() {
         return;
     }
     expression.secondNumber = parseFloat(box.value);
+
+    let firstNumber = checkForNegative(expression.firstNumber, singleKeyExpression.firstNumberNegative);
+    firstNumber = checkForPi(firstNumber, singleKeyExpression.firstNumberPi);
+    firstNumber = checkForPercent(firstNumber, singleKeyExpression.firstNumberPercent);
+    firstNumber = checkForSquareRoot(firstNumber, singleKeyExpression.firstNumberSquareRoot);
+    let secondNumber = checkForNegative(expression.secondNumber, singleKeyExpression.secondNumberNegative);
+    secondNumber = checkForPi(secondNumber, singleKeyExpression.secondNumberPi);
+    secondNumber = checkForPercent(secondNumber, singleKeyExpression.secondNumberPercent);
+    secondNumber = checkForSquareRoot(secondNumber, singleKeyExpression.secondNumberSquareRoot);
+
     if (expression.method === "plus") {
-        addNumbers(expression.firstNumber, expression.secondNumber);
+        addNumbers(firstNumber, secondNumber);
     } else if (expression.method === "minus") {
-        subtractNumbers(expression.firstNumber, expression.secondNumber);
+        subtractNumbers(firstNumber, secondNumber);
     } else if (expression.method === "multiply") {
-        multiplyNumbers(expression.firstNumber, expression.secondNumber);
+        multiplyNumbers(firstNumber, secondNumber);
     } else if (expression.method === "divide") {
-        divideNumbers(expression.firstNumber, expression.secondNumber);
+        divideNumbers(firstNumber, secondNumber);
     }
 }
 
@@ -120,6 +153,7 @@ function updateExpression(number) {
     expression.continuousText = false;
     expression.firstNumber = number;
     expression.secondNumber = "";
+    resetSingleKeys();
 }
 
 let memory = document.getElementById("memory");
@@ -150,4 +184,87 @@ document.getElementById("all-clear").addEventListener("click", () => {
 // function to check only allow numbers in text box
 function isTextInputANumber(e) {
     return e.which > 47 && e.which < 58 ? true : false;
+}
+// single key expressions
+document.getElementById("pi").addEventListener("click", () => {
+    box.value = Math.PI;
+    if (expression.editFirstNumber) {
+        singleKeyExpression.firstNumberPi = !singleKeyExpression.firstNumberPi;
+    } else {
+        singleKeyExpression.secondNumberPi = !singleKeyExpression.secondNumberPi;
+    }
+});
+
+document.getElementById("square").addEventListener("click", () => {
+    if (box.value === "") {
+        return;
+    }
+    box.value = Math.sqrt(box.value);
+    if (expression.editFirstNumber) {
+        singleKeyExpression.firstNumberSquareRoot = !singleKeyExpression.firstNumberSquareRoot;
+    } else {
+        singleKeyExpression.secondNumberSquareRoot = !singleKeyExpression.secondNumberSquareRoot;
+    }
+});
+
+document.getElementById("negative").addEventListener("click", () => {
+    if (box.value === "") {
+        box.value = "-";
+    } else {
+        box.value = -box.value;
+        if (expression.editFirstNumber) {
+            singleKeyExpression.firstNumberNegative = !singleKeyExpression.firstNumberNegative;
+        } else {
+            singleKeyExpression.secondNumberNegative = !singleKeyExpression.secondNumberNegative;
+        }
+    }
+});
+
+document.getElementById("percent").addEventListener("click", () => {
+    if (box.value === "") {
+        return;
+    }
+    box.value = parseFloat(box.value) * 0.01;
+    if (expression.editFirstNumber) {
+        singleKeyExpression.firstNumberPercent = !singleKeyExpression.firstNumberPercent;
+    } else {
+        singleKeyExpression.secondNumberPercent = !singleKeyExpression.secondNumberPercent;
+    }
+});
+
+function resetSingleKeys() {
+    singleKeyExpression.firstNumberPi = false;
+    singleKeyExpression.firstNumberNegative = false;
+    singleKeyExpression.firstNumberPercent = false;
+    singleKeyExpression.firstNumberSquareRoot = false;
+    singleKeyExpression.secondNumberPi = false;
+    singleKeyExpression.secondNumberNegative = false;
+    singleKeyExpression.secondNumberPercent = false;
+    singleKeyExpression.secondNumberSquareRoot = false;
+}
+
+function checkForNegative(number, singleKeyExpression) {
+    if (singleKeyExpression) {
+        number = -number;
+    }
+    return number;
+}
+
+function checkForPi(number, singleKeyExpression) {
+    if (singleKeyExpression) {
+        number = Math.PI;
+    }
+    return number;
+}
+function checkForPercent(number, singleKeyExpression) {
+    if (singleKeyExpression) {
+        number = number * 0.01;
+    }
+    return number;
+}
+function checkForSquareRoot(number, singleKeyExpression) {
+    if (singleKeyExpression) {
+        number = Math.sqrt(number);
+    }
+    return number;
 }
